0b7b82ff6d010169e77d8208cd9a3d4d56d7ed23
Natalia <natalia.iglesiast@gmail.com>
2019-10-17 16:16:53 +0200

    Model in Tariff - TariffForm

--- frontend/src/modules/tariffs/components/Tariff.js
+++ frontend/src/modules/tariffs/components/Tariff.js
@@ -5,21 +5,45 @@ import TariffForm from './TariffForm';
 import TariffItemList from './TariffItemList';
 import * as selectors from '../selectors';
 import * as actions from '../actions';
+import {SimpleModal} from '../../common';
+import {FormattedMessage} from 'react-intl';
 
 
 class Tariff extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {
+          showModal: false,
+        };
       }
+    
+    handleToggleModal() {
+        this.setState({ showModal: !this.state.showModal });
+    }
 
     componentDidMount() {
         this.props.findTariffs();
     }
 
     render(){
+
+        const { showModal } = this.state;
+
         return (
              <div>
-                <TariffForm history={this.props.history} />
+                 <div>
+                    <button type="button" className="btn  btn-link btn-sm"
+                    onClick={() => this.handleToggleModal()}>
+                     <span className="fas fa-plus" ></span> 
+                      &nbsp;
+                     <FormattedMessage id="project.tariffs.TariffForm.title"/>
+                    </button>
+
+                    {showModal &&
+                    <SimpleModal onCloseRequest={() => this.handleToggleModal()}>
+                        <TariffForm history={this.props.history} />
+                    </SimpleModal>}
+                    </div>
                 &nbsp;
                 &nbsp;
                 <TariffItemList list={this.props.tariffs} removeTariff= {this.props.removeTariff}/> 
