236c14db9f7e7794116abfb14ad91fe528422849
Natalia <natalia.iglesiast@gmail.com>
2019-10-23 12:31:08 +0200

    backend roomTypeService in Frontend

---
+++ frontend/src/backend/roomTypeService.js
@@ -0,0 +1,23 @@
+ 
+import {config, appFetch } from './appFetch';
+
+
+export const addRoomType = (roomType, onSuccess, onErrors) => 
+    appFetch('/roomTypes/addRoomType', config('POST', roomType), 
+    onSuccess, onErrors);
+
+export const findAllRoomTypes = (onSuccess,onErrors) => 
+    appFetch('/roomTypes/roomTypes', config('GET'),
+    onSuccess, onErrors);
+
+ export const updateRoomType = (roomType, onSuccess, onErrors) =>
+    appFetch(`/roomTypes/${roomType.id}`, config('PUT', roomType),
+    onSuccess, onErrors);
+
+export const removeRoomType = (roomType, onSuccess, onErrors) =>{
+    appFetch(`/roomTypes/${roomType.id}`, config('DELETE'), 
+    onSuccess, onErrors);}
+
+export const findRoomTypeById = (id, onSuccess, onErrors) =>{
+    appFetch(`/roomTypes/${id}`, config('GET'), 
+    onSuccess, onErrors);}
